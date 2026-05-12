18e5c6cd4327d89d8d9b2912690dc41421f3ba5f
Robinasch <robinaschwanden09@gmail.com>
2026-05-12 18:51:49 +0200

    RollDice mit zwei würfeln und augensumme hinzugefügt

--- demo_test.ts
+++ demo_test.ts
@@ -1,5 +1,5 @@
-import { assert } from "node:console";
-import { rollDie } from "./demo.ts";
+import { assert, assertEquals } from "@std/assert";
+import { rollDie, rollDice, diceSum } from "./demo.ts";
 
 Deno.test("rollDie gibt eine Zahl zwischen 1 und 6 zurück", () => {
   for (let i = 0; i < 100; i++) {
@@ -10,3 +10,17 @@ Deno.test("rollDie gibt eine Zahl zwischen 1 und 6 zurück", () => {
     assert(Number.isInteger(result));
   }
 });
+
+Deno.test("rollDie gibt zwei Würfel zurück", () => {
+    const dice = rollDice();
+    assertEquals(dice.length, 2);
+    assert(dice[0] >= 1);
+    assert(dice[0] <= 6);
+    assert(dice[1] >= 1);
+    assert(dice[1] <= 6);
+});
+
+Deno.test("diceSum berechnet Augensumme korrekt", () => {
+    const result = diceSum([3, 4]);
+    assertEquals(result, 7);
+})
